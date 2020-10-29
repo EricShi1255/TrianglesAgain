a34bd48fa439bae1a970ac0173f2df604d630818
EricShi1255 <eshi20@stuy.edu>
2020-10-28 20:18:12 -0400

    skeleton functions

---
+++ Point.java
@@ -0,0 +1,21 @@
+public class Point  {
+    private double x,y;
+    //construct a point given coordinates
+    public Point(double X, double Y){
+        x=X;
+        y=Y;
+    }
+     //construct a point given a point  (redundant , just provides flexibility)
+    public Point(Point p){
+        x= p.x; 
+        y= p.y;
+    }
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+        return y;
+    }  
+
+}
---
+++ PointTester.java
@@ -0,0 +1,7 @@
+public class PointTester {
+    public static void main (String[] args) {
+        Point a = new Point(3,4);
+        System.out.println(a.getX());
+        System.out.println(a.getY());
+    }
+}
